63b145b306261941405a3d635245969e293dfa32
lordgeorg <lordgeorg@googlemail.com>
2012-12-17 09:21:57 +0100

    added PrinteFormat class

---
+++ include/bprinter/PrintFormat.h
@@ -0,0 +1,37 @@
+#ifndef BPRINTER_PRINTFORMAT_H
+#define BPRINTER_PRINTFORMAT_H
+
+#include <string>
+
+namespace bprinter{
+
+enum PrintColor { black, red, green, yellow, blue, white};
+
+class PrintFormat{
+public:
+  PrintFormat(PrintColor color = black, bool bold = false) :
+    _color(color),
+    _bold(bold) {}
+  ~PrintFormat() {}
+
+  inline PrintColor color() const {
+    return _color;
+  }
+  inline bool bold() const {
+    return _bold;
+  }
+
+  std::string formatString() const;
+  std::string unformatString() const;
+
+private:
+  const PrintColor _color;
+  const bool _bold;
+};
+
+
+} //namespace bprinter
+
+
+#endif //BPRINTER_PRINTFORMAT_H
+
---
+++ src/PrintFormat.cpp
@@ -0,0 +1,30 @@
+#include "bprinter/PrintFormat.h"
+
+namespace bprinter{
+
+std::string PrintFormat::formatString() const {
+  std::string format("\033[");
+  switch(color()){
+    case red: format += "31"; break;
+    case green: format += "30"; break;
+    case yellow: format += "33"; break;
+    case blue: format += "30"; break;
+    case white: format += "30"; break;
+    case black:
+    default: format += "30";
+    
+  }
+  format += ";";
+  if (bold())
+    format += "1";
+  else
+    format += "0";
+
+  return format;
+}
+
+std::string PrintFormat::unformatString() const {
+  return std::string("\033[0");
+}
+
+} //namespace bprinter
